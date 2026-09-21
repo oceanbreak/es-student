dde2675fe7884798f1a72ac499aa5bbec9a0eecd
Ivan Anisimov <oceanbreak@gmail.com>
2026-09-22 00:02:03 +0300

    Сделана п1.3.6

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -1,6 +1,7 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 #include "led.h"
+#include "log.h"
 
 const uint BUTTON_PIN=15;
 const uint DEBOUNCE_MS = 20;
@@ -21,16 +22,23 @@ void handle_command(int command)
     if (command == 'e')
     {
         led_set(true);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+        // printf("led %s\n", led_is_on() ? "on" : "off");
     }
     else if (command == 'd')
     {
         led_set(false);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+        // printf("led %s\n", led_is_on() ? "on" : "off");
+    }
+    else if (command == 'v')
+    {
+        log_version();
     }
     else
     {
-        printf("unknown command %c\n", command);
+        LOG_ERR("unknown command %c\n", command);
+        // printf("unknown command %c\n", command);
     }
 }
 
@@ -51,7 +59,8 @@ int main()
         if (previous == true && current == false)
         {
             led_toggle();
-            printf("led %s\n", led_is_on() ? "on" : "off");
+            LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+            // printf("led %s\n", led_is_on() ? "on" : "off");
         }
 
         previous = current;
@@ -62,6 +71,7 @@ int main()
         {
             continue;
         }
+        LOG_DBG("got %c\n", command);
         handle_command(command);
 
     }
