4836833fb601d768b2e96807922830ad651c3ba9
Ivan Anisimov <oceanbreak@gmail.com>
2026-09-22 09:53:35 +0300

    Working on 1.3.7

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "led.h"
 #include "log.h"
+#include "device.h"
 
 const uint BUTTON_PIN=15;
 const uint DEBOUNCE_MS = 20;
@@ -32,6 +33,10 @@ void handle_command(int command)
     else if (command == 'v')
     {
         log_version();
+    }
+    else if (command == 'i')
+    {
+        device_info();
     }
     else
     {
